eb2bcf93e2150bd6501e61d245d4e65c9b4a5890
Umas Sunavan <fareastsunflower@gmail.com>
2022-09-26 17:37:31 +0800

    feat: day10: add example code

--- day10_map/index copy.js
+++ day10_map/index copy.js
@@ -1,9 +1,19 @@
+
+
 import * as THREE from 'three';
 import { OrbitControls } from 'https://unpkg.com/three@latest/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'https://unpkg.com/three@latest/examples/jsm/loaders/GLTFLoader';
+import { UnrealBloomPass } from 'https://unpkg.com/three@latest/examples/jsm/postprocessing/UnrealBloomPass';
+import { EffectComposer } from 'https://unpkg.com/three@latest/examples/jsm/postprocessing/EffectComposer';
+import { RenderPass } from 'https://unpkg.com/three@latest/examples/jsm/postprocessing/RenderPass';
+import { RectAreaLightHelper } from 'https://unpkg.com/three@latest/examples/jsm/helpers/RectAreaLightHelper.js';
+import { RectAreaLightUniformsLib } from 'https://unpkg.com/three@latest/examples/jsm/lights/RectAreaLightUniformsLib.js';
+
 
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
+// 已經存在的鏡頭位置設定
 camera.position.set(0, 10, 15)
 
 const renderer = new THREE.WebGLRenderer({antialias: true});
@@ -12,12 +22,12 @@ document.body.appendChild( renderer.domElement );
 
 // 匯入材質
 // image source: https://www.deviantart.com/kirriaa/art/Free-star-sky-HDRI-spherical-map-719281328
-const skydomeTexture = new THREE.TextureLoader().load('https://storage.googleapis.com/umas_public_assets/michaelBay/free_star_sky_hdri_spherical_map_by_kirriaa_dbw8p0w%20(1).jpg')
+const texture = new THREE.TextureLoader().load('star_sky_hdri_spherical_map_with_galaxy2 (3).jpg')
 // 帶入材質，設定內外面
-const skydomeMaterial = new THREE.MeshBasicMaterial( { map: skydomeTexture, side: THREE.DoubleSide})
-const skydomeGeometry = new THREE.SphereGeometry(100,50,50)
-const skydome = new THREE.Mesh(skydomeGeometry, skydomeMaterial);
-scene.add(skydome);
+const material = new THREE.MeshBasicMaterial( { map: texture, side: THREE.DoubleSide})
+const geometry = new THREE.SphereGeometry(100,50,50)
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
 
 // 新增環境光
 const addAmbientLight = () => {
@@ -32,7 +42,7 @@ const addPointLight = () => {
 	pointLight.position.set(10, 10, -10)
 	pointLight.castShadow = true
 	// 新增Helper
-	const lightHelper = new THREE.PointLightHelper(pointLight, 5, 0xffff00)
+	const lightHelper = new THREE.PointLightHelper(pointLight, 20, 0xffff00)
 	// scene.add(lightHelper);
 	// 更新Helper
 	lightHelper.update();
@@ -52,7 +62,7 @@ const addDirectionalLight = () => {
 	directionalLight.shadow.camera.bottom = - d;
 
 	// 新增Helper
-	const lightHelper = new THREE.DirectionalLightHelper(directionalLight, 5, 0xffff00)
+	const lightHelper = new THREE.DirectionalLightHelper(directionalLight, 20, 0xffff00)
 	// scene.add(lightHelper);
 	// 更新位置
 	directionalLight.target.position.set(0, 0, 0);
@@ -66,20 +76,19 @@ addAmbientLight()
 addDirectionalLight()
 
 const earthGeometry = new THREE.SphereGeometry(5,600,600)
+// 匯入材質
+// texture source: http://planetpixelemporium.com/earth8081.html
 const earthTexture = new THREE.TextureLoader().load('https://storage.googleapis.com/umas_public_assets/michaelBay/day10/8081_earthmap2k.jpg')
-// 灰階高度貼圖
 const displacementTexture = new THREE.TextureLoader().load('https://storage.googleapis.com/umas_public_assets/michaelBay/day10/editedBump.jpg')
-const roughtnessTexture = new THREE.TextureLoader().load('https://storage.googleapis.com/umas_public_assets/michaelBay/day10/8081_earthspec2kReversedLighten.png')
-const speculatMapTexture = new THREE.TextureLoader().load('https://storage.googleapis.com/umas_public_assets/michaelBay/day10/8081_earthspec2k.jpg')
+const speculatMapTextureReversed = new THREE.TextureLoader().load('https://storage.googleapis.com/umas_public_assets/michaelBay/day10/8081_earthspec2kReversedLighten.png')
 const bumpTexture = new THREE.TextureLoader().load('https://storage.googleapis.com/umas_public_assets/michaelBay/day10/8081_earthbump2k.jpg')
-
-
+const speculatMapTexture = new THREE.TextureLoader().load('https://storage.googleapis.com/umas_public_assets/michaelBay/day10/8081_earthspec2k.jpg')
+// 帶入材質，設定內外面
 const earthMaterial = new THREE.MeshStandardMaterial( { 
 	map: earthTexture, 
 	side: THREE.DoubleSide, 
-	roughnessMap:roughtnessTexture,
+	roughnessMap:speculatMapTextureReversed,
 	roughness:0.9,
-	// 將貼圖貼到材質參數中
 	metalnessMap: speculatMapTexture,
 	metalness:1,
 	displacementMap:displacementTexture,
@@ -93,7 +102,7 @@ scene.add(earth);
 const cloudGeometry = new THREE.SphereGeometry(5.4,60,60)
 // 匯入材質
 // texture source: http://planetpixelemporium.com/earth8081.html
-const cloudTransparency = new THREE.TextureLoader().load('https://storage.googleapis.com/umas_public_assets/michaelBay/day10/8081_earthhiresclouds2K.jpg')
+const cloudTransparency = new THREE.TextureLoader().load('8081_earthhiresclouds4K.jpg')
 // 帶入材質，設定內外面
 const cloudMaterial = new THREE.MeshStandardMaterial( { 
 	transparent: true, 
@@ -103,19 +112,18 @@ const cloudMaterial = new THREE.MeshStandardMaterial( {
 const cloud = new THREE.Mesh(cloudGeometry, cloudMaterial);
 scene.add(cloud);
 
-
-// 帶入鏡頭跟renderer.domElement實例化它即可
-new OrbitControls( camera, renderer.domElement );
+// 將OrbitControls存於一個變數
+const control = new OrbitControls( camera, renderer.domElement );
 
 const axesHelper = new THREE.AxesHelper( 5 );
 scene.add( axesHelper );
-
+control.update()
 function animate() {
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
-	earth.rotation.y +=0.005
-	cloud.rotation.y +=0.004
-	skydome.rotation.y += 0.001
+	// earth.rotation.y +=0.005
+	// cloud.rotation.y +=0.004
+	// sphere.rotation.y += 0.001
 
 }
 animate();
