c412495b94de0b4f22583690581c72e69697608c
Umas Sunavan <fareastsunflower@gmail.com>
2023-08-17 01:02:29 +0800

    feat: add ch8

---
+++ ch8_light_showcase/index.js
@@ -0,0 +1,139 @@
+import * as THREE from "three";
+import { OrbitControls } from "https://unpkg.com/three@latest/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "https://unpkg.com/three@latest/examples/jsm/loaders/GLTFLoader";
+import { RectAreaLightHelper } from "https://unpkg.com/three@latest/examples/jsm/helpers/RectAreaLightHelper.js";
+import { RectAreaLightUniformsLib } from "https://unpkg.com/three@latest/examples/jsm/lights/RectAreaLightUniformsLib.js";
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  20,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  100
+);
+camera.zoom = 0.4;
+camera.updateProjectionMatrix();
+camera.position.set(5, 5, 10);
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.shadowMap.enabled = true;
+
+const sphereGeometry = new THREE.SphereGeometry(50, 30, 30);
+const planeMaterial = new THREE.MeshStandardMaterial({
+  side: THREE.BackSide,
+  color: 0xcceeff,
+});
+const sphere = new THREE.Mesh(sphereGeometry, planeMaterial);
+sphere.position.set(0, 0, 0);
+scene.add(sphere);
+
+new GLTFLoader().load(
+  "https://storage.googleapis.com/umas_public_assets/michaelBay/file.gltf",
+  (gltf) => {
+    gltf.scene.traverse((object) => {
+      if (object.isMesh) {
+        object.material.roughness = 1;
+        object.material.metalness = 0;
+        object.material.transparent = false;
+        object.castShadow = true;
+        object.receiveShadow = true;
+      }
+    });
+    scene.add(gltf.scene);
+  }
+);
+
+// 新增點光
+const addPointLight = () => {
+  const pointLight = new THREE.PointLight(0xffffff, 20);
+  scene.add(pointLight);
+  pointLight.position.set(3, 3, 0);
+  pointLight.castShadow = true;
+  // 新增Helper
+  const lightHelper = new THREE.PointLightHelper(pointLight, 5, 0xffff00);
+  scene.add(lightHelper);
+  // 更新Helper
+  lightHelper.update();
+};
+
+// 新增平行光
+const addDirectionalLight = () => {
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.4);
+  directionalLight.position.set(20, 20, 0);
+  scene.add(directionalLight);
+  directionalLight.castShadow = true;
+  const d = 10;
+
+  directionalLight.shadow.camera.left = -d;
+  directionalLight.shadow.camera.right = d;
+  directionalLight.shadow.camera.top = d;
+  directionalLight.shadow.camera.bottom = -d;
+
+  // 新增Helper
+  const lightHelper = new THREE.DirectionalLightHelper(
+    directionalLight,
+    20,
+    0xffff00
+  );
+  // scene.add(lightHelper);
+  // 更新位置
+  directionalLight.target.position.set(0, 0, 0);
+  directionalLight.target.updateMatrixWorld();
+  // 更新Helper
+  lightHelper.update();
+};
+
+// 聚光燈
+const addSpotLight = () => {
+  const spotLight = new THREE.SpotLight(0xffffff, 10);
+  spotLight.position.set(3, 3, 0);
+  spotLight.castShadow = true;
+  scene.add(spotLight);
+};
+
+// 區域光
+const addRectAreaLight = () => {
+  const width = 10;
+  const height = 10;
+  const intensity = 1;
+  const rectLight = new THREE.RectAreaLight(0xffffff, 1, width, height);
+  rectLight.position.set(5, 5, 0);
+  rectLight.lookAt(0, 0, 0);
+  scene.add(rectLight);
+
+  const rectLightHelper = new RectAreaLightHelper(rectLight);
+  rectLight.add(rectLightHelper);
+};
+
+// 半球光
+const addHemisphereLight = () => {
+  const hemisphereLight = new THREE.HemisphereLight(0xffff99, 0x9999ff, 1);
+  scene.add(hemisphereLight);
+};
+
+// 新增環境光
+const addAmbientLight = () => {
+  const light = new THREE.AmbientLight(0xffffff, 0.4);
+  scene.add(light);
+};
+
+const control = new OrbitControls(camera, renderer.domElement);
+control.target.set(0, 2, 3);
+control.update();
+
+addSpotLight();
+addPointLight();
+addDirectionalLight();
+addAmbientLight();
+addRectAreaLight();
+addHemisphereLight();
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+animate();
---
+++ ch8_normal_example/index.js
@@ -0,0 +1,62 @@
+import * as THREE from "three";
+import { OrbitControls } from "https://unpkg.com/three@latest/examples/jsm/controls/OrbitControls.js";
+
+const scene = new THREE.Scene();
+// PerspectiveCamera 需設定四個參數，下面接著介紹
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+// 修改鏡頭位置
+camera.position.set(0, 0, 15);
+// 實例化渲染器
+const renderer = new THREE.WebGLRenderer();
+// 渲染器負責投影畫面在螢幕上，會需要寬高
+renderer.setSize(window.innerWidth, window.innerHeight);
+// 帶入鏡頭跟renderer.domElement實例化它即可
+new OrbitControls(camera, renderer.domElement);
+// 渲染器會產生canvas物件，我們在html的body放置它
+document.body.appendChild(renderer.domElement);
+
+// 改成球體
+const geometry = new THREE.SphereGeometry(100, 50, 50); // 參數帶入半徑、水平面數、垂直面數
+// 匯入材質
+const texture = new THREE.TextureLoader().load(
+  "https://storage.googleapis.com/umas_public_assets/michaelBay/starmap_4k.jpeg"
+);
+const material = new THREE.MeshStandardMaterial({
+  map: texture,
+  side: THREE.DoubleSide,
+});
+// 新增環境光
+const light = new THREE.AmbientLight(0xffffff, 1);
+scene.add(light);
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
+const earthGeometry = new THREE.SphereGeometry(5, 50, 50);
+// 匯入材質
+// Author/Origin: NASA Jet Propulsion Laboratory - Solar System Simulator
+const earthTexture = new THREE.TextureLoader().load(
+  "https://storage.googleapis.com/umas_public_assets/michaelBay/ch07/earth_map.jpg"
+);
+// 帶入材質，設定內外面
+const earthMaterial = new THREE.MeshStandardMaterial({
+  map: earthTexture,
+  side: THREE.DoubleSide,
+});
+const earth = new THREE.Mesh(earthGeometry, earthMaterial);
+scene.add(earth);
+// 宣告旋轉變數
+const cameraLookAt = new THREE.Vector3(10, 0, 0);
+// 移動到animate()之外
+camera.lookAt(cameraLookAt);
+let rotation = 0;
+function animate() {
+  requestAnimationFrame(animate);
+  // 每一幀，場景物件都會被鏡頭捕捉
+  renderer.render(scene, camera);
+}
+// 函式起始點
+animate();
