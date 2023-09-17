ee67f7bf024feb3d8ba615e9dbc789bf8f310ef8
Umas Sunavan <fareastsunflower@gmail.com>
2023-09-17 15:11:00 +0800

    feat: add ch25

---
+++ ch25_complete/index.js
@@ -0,0 +1,48 @@
+import * as THREE from "three";
+import { OrbitControls } from "https://unpkg.com/three@latest/examples/jsm/controls/OrbitControls.js";
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xf2f2f2);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  1,
+  10000
+);
+camera.position.set(0, 30, 30);
+new OrbitControls(camera, renderer.domElement);
+
+const addSphere = () => {
+  const vertex = document.getElementById("vertexShader").innerHTML;
+  const fragment = document.getElementById("fragmentShader").innerHTML;
+  const geo = new THREE.SphereGeometry(5, 15, 15);
+  const mat = new THREE.ShaderMaterial({
+    vertexShader: vertex,
+    fragmentShader: fragment,
+    uniforms: {
+      uIntensity: {
+        value: 0.5, // 實作下一步input事件之前先給它預設值
+      },
+    },
+  });
+  const mesh = new THREE.Mesh(geo, mat);
+  scene.add(mesh);
+  return mesh;
+};
+document.getElementById("intensity").addEventListener("input", (event) => {
+  // console.log(+event.target.value)
+  console.log(sphere.material.uniforms.uIntensity.value);
+  sphere.material.uniforms.uIntensity.value = +event.target.value;
+});
+
+const sphere = addSphere();
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+animate();
